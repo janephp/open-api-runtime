b12c98f255d687a1fd5fdaae80d7578fe8162500
Loïck Piera <pyrech@gmail.com>
2019-09-20 10:16:58 +0200

    Add support for PSR18 client generation

--- Tests/Client/Psr18ClientTest.php
+++ Tests/Client/Psr18ClientTest.php
@@ -4,51 +4,67 @@ declare(strict_types=1);
 
 namespace Jane\OpenApiRuntime\Tests\Client;
 
-use Http\Client\HttpClient;
-use Http\Message\MessageFactory;
-use Http\Message\StreamFactory;
-use Jane\OpenApiRuntime\Client\Psr7HttplugClient;
+use Jane\OpenApiRuntime\Client\Psr18Client;
 use Jane\OpenApiRuntime\Client\Psr7Endpoint;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class Psr7HttplugClientTest extends TestCase
+class Psr18ClientTest extends TestCase
 {
     /**
-     * @covers \Jane\OpenApiRuntime\Client\Psr7HttplugClient::__construct
-     * @covers \Jane\OpenApiRuntime\Client\Psr7HttplugClient::executePsr7Endpoint
+     * @covers \Jane\OpenApiRuntime\Client\Psr18Client::__construct
+     * @covers \Jane\OpenApiRuntime\Client\Psr18Client::executePsr7Endpoint
      */
     public function testExecutePsr7Endpoint()
     {
         $responseMock = $this->createMock(ResponseInterface::class);
-        $httpClientMock = $this->createMock(HttpClient::class);
+        $httpClientMock = $this->createMock(ClientInterface::class);
         $httpClientMock
             ->expects($this->once())
             ->method('sendRequest')
             ->willReturn($responseMock);
 
         $requestMock = $this->createMock(RequestInterface::class);
-        $messageFactoryMock = $this->createMock(MessageFactory::class);
-        $messageFactoryMock
+
+        $requestFactoryMock = $this->createMock(RequestFactoryInterface::class);
+        $requestFactoryMock
             ->expects($this->once())
             ->method('createRequest')
-            ->with('GET', '/foo', [], 'bar')
+            ->with('GET', '/foo')
             ->willReturn($requestMock);
 
         $serializerMock = $this->createMock(SerializerInterface::class);
 
-        $streamFactoryMock = $this->createMock(StreamFactory::class);
+        $streamFactoryMock = $this->createMock(StreamFactoryInterface::class);
 
-        $client = new class($httpClientMock, $messageFactoryMock, $serializerMock, $streamFactoryMock) extends Psr7HttplugClient {
+        $client = new class($httpClientMock, $requestFactoryMock, $serializerMock, $streamFactoryMock) extends Psr18Client {
         };
+
+        $streamMock = $this->createMock(StreamInterface::class);
+
+        $requestMock
+            ->expects($this->once())
+            ->method('withBody')
+            ->with($streamMock)
+            ->willReturn($requestMock);
+
+        $streamFactoryMock
+            ->expects($this->once())
+            ->method('createStream')
+            ->with('foo')
+            ->willReturn($streamMock);
 
         $endpointMock = $this->getMockBuilder(Psr7Endpoint::class)->getMock();
         $endpointMock
             ->expects($this->once())
             ->method('getBody')
-            ->willReturn([[], 'bar']);
+            ->willReturn([[], 'foo']);
         $endpointMock
             ->expects($this->once())
             ->method('getQueryString')
